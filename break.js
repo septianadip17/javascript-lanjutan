2c244f05136ffecc39a03abab298955659807b80
Septian Adi Pratama <septianadip7998@gmail.com>
2023-12-08 00:57:06 +0700

    test

---
+++ break.js
@@ -0,0 +1,4 @@
+for (var i = 0; i < 10; i++) {
+    if (i == 5) { break; }          // stops and exits the cycle
+    document.write(i + ", ");       // last output number is 4
+    }
---
+++ return.js
@@ -0,0 +1,19 @@
+// function tambah(angka1, angka2) {
+//     return angka1 * angka2;
+// }
+// var hasil = tambah(5, 3);
+// console.log(hasil);
+
+function adalahGenap(angka) {
+    if (angka % 2 === 0) {
+        return true; 
+    } else {
+        return false;
+    }
+}
+
+let hasil1 = adalahGenap(4); 
+let hasil2 = adalahGenap(7); 
+
+console.log(hasil1);
+console.log(hasil2);
